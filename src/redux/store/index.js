8baed34a458efb4bc1e19aca6d3e3dbc23d3497e
Muhammad Osama Khan <122485672+usamakhan12345@users.noreply.github.com>
2024-01-03 10:41:32 +0500

    done

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -14,7 +14,7 @@ const persistConfig = {
     key: 'root',
     storage,
   }
-   
+    
   const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = configureStore({
     reducer: {
